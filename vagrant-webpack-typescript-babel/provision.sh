fadd46e15561685284747ddaa38335fbc6064394
mventura <mventura@voyageprive.com>
2017-03-21 22:36:30 +0100

    init

---
+++ vagrant-webpack-typescript-babel/provision.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+
+#nodejs and npm
+sudo apt-get install curl python-software-properties
+curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
+sudo apt-get install nodejs
+
